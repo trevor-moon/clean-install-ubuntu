4178b9673069f5396a07b4ef768001afe3e4657b
Trevor Moon <trevor.r.moon@gmail.com>
2022-02-23 21:19:37 -0500

    add vscodium apt install script

---
+++ apt/install_codium.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# apt-install codium from .deb
+
+# add GPG key to repo
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+
+# add the repo
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+
+# update and install
+sudo apt update && sudo apt install codium
